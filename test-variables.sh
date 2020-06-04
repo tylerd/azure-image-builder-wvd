c92fdfeba0c160f0a50e21d71d895f908cc8f085
Mariusz Kolodziej <mariusz.kolodziej@outlook.com>
2020-06-04 13:21:53 -0400

    test

--- test-variables.sh
+++ test-variables.sh
@@ -3,7 +3,7 @@ printenv
 echo '-----------------------------'
 echo $1
 echo 'Hello World!'
-echo $IMAGERESORUCEGROUP #four
+echo $IMAGERESOURCEGROUP #four
 echo $imageresourcegroup
 ${{ variables.imageresourcegroup }}
 echo '2 - ' $(imageresourcegroup) # two
