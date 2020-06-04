9c4078cda36e2fcb1bf67d9728fcd07fea57ac48
Mariusz Kolodziej <mariusz.kolodziej@outlook.com>
2020-06-04 13:27:02 -0400

    final test please!

--- test-variables.sh
+++ test-variables.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
-printenv
-echo '-----------------------------'
-echo $1
-echo 'Hello World!'
-echo $IMAGERESOURCEGROUP #four
-echo $imageresourcegroup
-${{ variables.imageresourcegroup }}
-echo '2 - ' $(imageresourcegroup) # two
-echo '3 - ' $imageresourcegroup # three
+# printenv
+# echo '-----------------------------'
+# echo $1
+# echo 'Hello World!'
+# echo $IMAGERESOURCEGROUP #four
+# echo $imageresourcegroup
+# ${{ variables.imageresourcegroup }}
+# echo '2 - ' $(imageresourcegroup) # two
+# echo '3 - ' $imageresourcegroup # three
 echo '4 - ' $IMAGERESORUCEGROUP #four
+
+az group show --name $IMAGERESORUCEGROUP
