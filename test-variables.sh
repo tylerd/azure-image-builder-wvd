97d498ec2a003fdd10b9f1642ba8a0cc67e7d028
Mariusz Kolodziej <mariusz.kolodziej@outlook.com>
2020-06-04 11:45:31 -0400

    testing vars

--- test-variables.sh
+++ test-variables.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo '1 - '${{ variables.imageResourceGroup }} # one
-echo '2 - '$(imageResourceGroup) # two
-echo '3 - '$imageResourceGroup # three
+echo '1 - '${{ variables.image_resource_group }} # one
+echo '2 - '$(image_resource_group) # two
+echo '3 - '$image_resource_group # three
 echo '4 - '$IMAGE_RESORUCE_GROUP #four
