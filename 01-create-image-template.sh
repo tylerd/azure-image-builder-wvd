94c787286cce3217ae45e1f895078101c5ffd6d2
Mariusz Kolodziej <mariusz.kolodziej@outlook.com>
2020-06-04 11:15:15 -0400

    testing vars

--- 01-create-image-template.sh
+++ 01-create-image-template.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+echo $(image_resource_group)
+
 # Create the image template
 az resource create --verbose \
-    --resource-group $(image_resource_group) \
+    --resource-group echo $(image_resource_group) \
     --properties @helloImageTemplateWin.json \
     --is-full-object \
     --resource-type Microsoft.VirtualMachineImages/imageTemplates \
