33f9ccd107d7f6f33c1ea0f3b525cef43f564cbc
Mariusz Kolodziej <mariusz.kolodziej@outlook.com>
2020-06-04 14:49:06 -0400

    test

--- test-variables.sh
+++ test-variables.sh
@@ -6,10 +6,12 @@ echo '-----------------------------'
 # echo $1
 # echo 'Hello World!'
 echo $IMAGERESOURCEGROUP
+echo '-----------------------------'
+echo ${IMAGERESOURCEGROUP}
 # echo $imageresourcegroup
 # ${{ variables.imageresourcegroup }}
 # echo '2 - ' $(imageresourcegroup) # two
 # echo '3 - ' $imageresourcegroup # three
 echo '4 - ' $IMAGERESORUCEGROUP #four
 
-az group show --name {$IMAGERESORUCEGROUP}
+az group show --name ${IMAGERESORUCEGROUP}
