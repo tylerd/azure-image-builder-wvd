0e7a51bf2e9eb01f0c726754dd76af1c3299eb28
Natalia Konokhova <nataliak@microsoft.com>
2020-06-22 13:25:40 -0400

    added nested path and resource group name,

--- build-host-pool.sh
+++ build-host-pool.sh
@@ -2,22 +2,24 @@
 expiretime=$(date -u -d '30 minutes' +%Y-%m-%dT%H:%MZ)
 storageAccountRG="Azureminilab-Lighthouse"
 storageAccountName="azminlandevops"
+rootcontainer="templates"
+container="arm-templates"
 
 connection=$(az storage account show-connection-string \
   --resource-group $storageAccountRG \
   --name $storageAccountName \
   --query connectionString)
 token=$(az storage container generate-sas \
-  --name arm-templates \
+  --name $rootcontainer \
   --expiry $expiretime \
   --permissions rw \
   --output tsv \
   --connection-string $connection)
 
   url=$(az storage blob url \
-   --container-name arm-templates \
+   --container-name $rootcontainer/$container \
    --name main.json \
    --output tsv \
    --connection-string $connection)
  parameter='{"_artifactsLocationSasToken":{"value":"?'$token'"}}'
- az deployment group create --resource-group ExampleGroup --template-uri $url?$token --parameters $parameter
+ az deployment group create --resource-group ${SESSIONHOSTRESOURCEGROUPNAME} --template-uri $url?$token --parameters $parameter
