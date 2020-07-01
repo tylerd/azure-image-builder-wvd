300314502c50058ebbac0cfe65a1b0de248aaf4e
Natalia Konokhova <nataliak@microsoft.com>
2020-07-01 12:49:52 -0400

    host pool compute

---
+++ New-DiskEncryptSet.ps1
@@ -0,0 +1,36 @@
+#Requires -Modules Az.Storage
+#Requires -Modules Az.Resources
+
+$startTime = Get-Date
+$expiryTime = $startTime.AddMinutes(30)
+$StorageAccountRG = "aib-devops-rg"
+$StorageAccountName = "azminlandevops"
+$RootContainer = "templates"
+$folder = "arm-templates"
+
+$MainTemplate = "new-diskencryptset.json"
+
+$TargetResourceGroup = "Azureminilab-WVD-DES"
+$Location="canadacentral"
+
+$rg = Get-AzResourceGroup -Name $TargetResourceGroup -ErrorAction SilentlyContinue
+if (!$rg) {
+    $rg = New-AzResourceGroup -Name $TargetResourceGroup -Location $Location
+}
+
+Set-AzCurrentStorageAccount -ResourceGroupName $StorageAccountRG -Name $StorageAccountName
+
+$token = New-AzStorageContainerSASToken -Name $RootContainer -ExpiryTime $expiryTime -Permission "r"
+
+$templateUri = (Get-AzStorageBlob -Blob "$folder/$MainTemplate" -Container $RootContainer).ICloudBlob.Uri.AbsoluteUri
+
+$secureToken = ConvertTo-SecureString -String $token -AsPlainText -Force
+
+<#New-AzResourceGroupDeployment -ResourceGroupName $TargetResourceGroup `
+    -TemplateUri "$templateUri$token" -Verbose `
+    -artifactsLocationSasToken $secureToken
+    #>
+
+    New-AzResourceGroupDeployment -ResourceGroupName $TargetResourceGroup `
+    -TemplateFile $MainTemplate -Verbose `
+    -artifactsLocationSasToken $secureToken
