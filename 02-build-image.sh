4f46ffedcac4d3b97c753828f5c44eca219c80c5
Tyler Doerksen <tylerd@microsoft.com>
2020-07-21 16:18:12 -0500

    removing unused files

--- 02-build-image.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Start the image build
-az resource invoke-action \
-     --verbose \
-     --resource-group ${IMAGERESORUCEGROUP} \
-     --resource-type  Microsoft.VirtualMachineImages/imageTemplates \
-     -n ${IMAGERESOURCENAME} \
-     --action Run
--- 03-delete-image-template.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Delete the image template
-az resource delete \
-    --verbose \
-    --resource-group ${IMAGERESORUCEGROUP} \
-    --resource-type Microsoft.VirtualMachineImages/imageTemplates \
-    -n 01-image-template-win-10-desktop
